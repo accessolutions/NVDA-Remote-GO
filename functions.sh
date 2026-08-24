470ccc45913718ce7f1cb207c3cae2085c8eeb02
daviddamota80 <d.damota@accessolutions.fr>
2026-08-24 22:15:53 +0200

    Corrige la version des builds sans tag

--- functions.sh
+++ functions.sh
@@ -7,7 +7,12 @@ exit 10
 fi
 }
 git_version() {
-git describe --tags $(git rev-list --tags --max-count=1)
+version=$(git describe --tags "$(git rev-list --tags --max-count=1)" 2>/dev/null || true)
+if [ -n "$version" ]; then
+echo "$version"
+else
+git rev-parse --short HEAD
+fi
 }
 gen_cert() {
 check go run . -launch=false -log-level=-1 -conf-read=false -gen-cert-file $(pwd)/cert.pem
